fc56c276a2bef02ecead9c315352050295a78af0
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-09-15 10:50:57 +0530

    Setup route and added dashboard page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([])
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
