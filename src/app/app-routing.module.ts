08eb870717351493696e360900ad35dd98b3e85b
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-09-15 15:57:29 +0530

    Added Login UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { CustomerEditComponent } from './customer/customer-edit/customer-edit.component';
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
+import { NetbankingComponent } from './netbanking/netbanking.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'netbanking', component: NetbankingComponent },
+  { path: 'customers', component: CustomerListComponent },
+  { path: 'customers/edit/:id', component: CustomerEditComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'dashboard' }
 ]
