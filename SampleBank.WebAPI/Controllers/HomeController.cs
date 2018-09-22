886065281214ed6385d5ca5dfb9eedaa2fb8b0d6
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-09-22 15:44:30 +0530

    WebAPI initial setup

---
+++ SampleBank.WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace SampleBank.WebAPI.Controllers
+{
+    [Route("customers")]
+    public class CustomerController : ApiController
+    {
+        [Route(":id")]
+        public string Get(int id)
+        {
+            return "Test "+ id.ToString();
+        }
+    }
+}
