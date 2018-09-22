86cf263ab5d4cde5de99b64af9f8ee87d7fce10b
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-09-22 22:09:05 +0530

    Ran on Windows machine

---
+++ SampleBank.WebAPI/Backup/Controllers/CustomerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace SampleBank.WebAPI.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        public string Get()
+        {
+            return "Test";
+        }
+    }
+}
