d1c899ee3f10403dc9ffd6d11896769afcea70e5
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-10-16 23:57:04 +0530

    Updated angular cli to latest rc version.

--- sample-bank-start/src/app/customer/customer-list/customer-list.component.ts
+++ sample-bank-start/src/app/customer/customer-list/customer-list.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerListComponent implements OnInit {
 
-  constructor() { }
+  // constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
