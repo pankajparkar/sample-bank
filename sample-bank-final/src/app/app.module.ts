24a56724b5ce51092f767a97a412e37b95f3bc6c
Pankaj Parkar <parkarpankaj3@gmail.com>
2018-10-22 23:01:33 +0530

    Copied start folder to final.

--- sample-bank-final/src/app/app.module.ts
+++ sample-bank-final/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,33 @@ import { CustomerEditComponent } from './customer/customer-edit/customer-edit.co
 import { CustomerListComponent } from './customer/customer-list/customer-list.component';
 import { NetbankingComponent } from './netbanking/netbanking.component';
 
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'indianCurrency'})
+export class IndianCurrency implements PipeTransform {
+  transform(value: number, args: string[]): any {
+
+        if (! isNaN(value)) {
+            var currencySymbol = '₹';
+            //var output = Number(input).toLocaleString('en-IN');   <-- This method is not working fine in all browsers!           
+            var result = value.toString().split('.');
+
+            var lastThree = result[0].substring(result[0].length - 3);
+            var otherNumbers = result[0].substring(0, result[0].length - 3);
+            if (otherNumbers != '')
+                lastThree = ',' + lastThree;
+            var output = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
+
+            if (result.length > 1) {
+                output += "." + result[1];
+            }            
+
+            return currencySymbol + output;
+        }
+
+  }
+}
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,10 +49,12 @@ import { NetbankingComponent } from './netbanking/netbanking.component';
     CustomerEditComponent,
     CustomerListComponent,
     NetbankingComponent,
+    IndianCurrency
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
